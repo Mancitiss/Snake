5d292e069db9cde73e5995529223ff282a12319c
Mancitiss <vovuongthanhtu@gmail.com>
2021-02-24 13:05:52 +0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -2,8 +2,9 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
 class snakeApp(App):
-	a = GridLayout()
-	return a
+	def build(self):
+		a = GridLayout()
+		return a
 
 if __name__ == '__main__':
 	snakeApp().run()
