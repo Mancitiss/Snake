30d7bd31d6cc7f96bdb6cc3295611ceac494074f
Mancitiss <vovuongthanhtu@gmail.com>
2021-02-24 14:20:04 +0700

    add Grid
    test how Grids work with colors

--- snake.py
+++ snake.py
@@ -1,16 +1,21 @@
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy_addons.CustomModules import CustomGraphics
+from random import random
+
+#CustomGraphics.SetBG(layout, bg_color=[1,0,0,1])
 
 class snakeApp(App):
 	def build(self):
-		a = GridLayout(cols = 1)
-		b = GridLayout(cols = 1)
-		c = GridLayout(cols = 1)
-		CustomGraphics.SetBG(b, bg_color = [0, 1, 0, 1])
-		CustomGraphics.SetBG(c, bg_color = [0, 0, 1, 1])
-		a.add_widget(b)
-		a.add_widget(c)
+		a = GridLayout(cols = 50)
+		
+		list_a = [GridLayout() for _ in range(2500)]
+		for c in list_a:
+			CustomGraphics.SetBG(c, bg_color = [random(), random(), random(), 1])
+			a.add_widget(c)
 		return a
 
 if __name__ == '__main__':
