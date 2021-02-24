561425b112e15fe2dca6916ab293e40cf401206f
Mancitiss <vovuongthanhtu@gmail.com>
2021-02-24 13:37:47 +0700

    color test
    mainly to test how colors work

--- snake.py
+++ snake.py
@@ -1,9 +1,16 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy_addons.CustomModules import CustomGraphics
 
 class snakeApp(App):
 	def build(self):
-		a = GridLayout()
+		a = GridLayout(cols = 1)
+		b = GridLayout(cols = 1)
+		c = GridLayout(cols = 1)
+		CustomGraphics.SetBG(b, bg_color = [0, 1, 0, 1])
+		CustomGraphics.SetBG(c, bg_color = [0, 0, 1, 1])
+		a.add_widget(b)
+		a.add_widget(c)
 		return a
 
 if __name__ == '__main__':
